01d3e5ab48f55796378fd5fae6e1492ed6fc363b
Andrew Sanchez <druu831@gmail.com>
2024-01-11 15:10:16 -0800

    feat. added prisma. neon db connection. user & account model. and the ability to register a user

--- actions/register.ts
+++ actions/register.ts
@@ -1,14 +1,37 @@
 "use server";
 
-import { LoginSchema } from "@/schemas";
+import { db } from "@/lib/db";
+import { RegisterSchema } from "@/schemas";
 import * as z from "zod";
+import bcrypt from "bcrypt";
+import { getUserByEmail } from "@/data/user";
 
-export async function register(values: z.infer<typeof LoginSchema>) {
-  const validatedFields = LoginSchema.safeParse(values);
+export async function register(values: z.infer<typeof RegisterSchema>) {
+  const validatedFields = RegisterSchema.safeParse(values);
 
   if (!validatedFields.success) {
     return { error: "Invalid fields" };
   }
 
-  return { success: "Register success" };
+  const { email, password, firstName, lastName } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const existingUser = await getUserByEmail(email);
+
+  if (existingUser) {
+    return { error: "Email already in use" };
+  }
+
+  await db.user.create({
+    data: {
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: Send verification token email
+
+  return { success: "User created successfully" };
 }
